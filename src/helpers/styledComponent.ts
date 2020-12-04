ff47f508ec4dca6c17e4167cc6974154658fbbfd
Matt McCray <darthapo@gmail.com>
2020-12-04 00:56:19 -0600

    Support for naming styled components.

--- src/helpers/styledComponent.ts
+++ src/helpers/styledComponent.ts
@@ -2,42 +2,55 @@ import _ from "lodash";
 import html, { css } from "../core/render.js";
 import registerContent from "../core/registry.js";
 
+
+
 interface StyledComponent {
   (props?: { className?: string }, children?: string): string,
   className: string
+  componentName: string
 }
 
 interface StyledComponentBuilder {
-  (tag: string, styles: string | ((cls: string) => string)): StyledComponent;
+  (name: string, tag: string, styles: string | ((cls: string) => string)): StyledComponent;
   [tag: string]: (strings: TemplateStringsArray, ...values: any[]) => StyledComponent
 }
 
-function styled_(tag: string, styles: string | ((cls: string) => string)): StyledComponent {
-  const style_id = _.uniqueId(tag + '_')
+function styled_(name: string, tag: string, styles: string | ((cls: string) => string)): StyledComponent {
+  const innerCls = generateClassName(name, tag) //_.uniqueId(name + '_')
+  const clsName = name === tag ? innerCls : `${name} ${innerCls}`
   const css = (typeof styles === 'function')
-    ? styles(`.${style_id}`)
-    : `.${style_id} { ${styles} }`
+    ? styles(`.${innerCls}`)
+    : `.${innerCls} { ${styles} }`
 
   registerContent.stylesheet(css)
 
-  const Component = (props?: { className?: string }, children?: any) => {
+
+  const Component = function (props?: { className?: string }, children?: any) {
     const className = (!!props && 'className' in props) ? props.className : ''
-    return html`<${tag} class="${style_id} ${className}">${children}</${tag}>`
+    return html`<${tag} class="${clsName} ${className}">${children}</${tag}>`
   }
-  Component.className = style_id
+  Component.componentName = name || 'Anonymous'
+  Component.className = innerCls
 
   return Component
 }
 
 export const styled: StyledComponentBuilder = new Proxy(styled_, {
   get(target, tag, rec) {
-    return (strings: TemplateStringsArray, ...values: any[]) => styled_(String(tag), css(strings, ...values))
+    return (strings: TemplateStringsArray, ...values: any[]) => styled_(String(tag), String(tag), css(strings, ...values))
   }
 }) as StyledComponentBuilder
 
 
 export default styled
 
+
+let classCount = 177000 // 100000
+
+function generateClassName(name?: string, tag?: string) {
+  classCount += 1
+  return '_' + classCount.toString(26) //.toString(36)
+}
 
 // const Test = styled.div`
 //   color: red;
